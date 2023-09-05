d5e4685f0e6ba100ef64d210c96db9e5da19e21e
Sean Finnegan <seandgfinnegan@gmail.com>
2023-09-06 00:45:11 +1000

    Changing to plugin markdown editor

---
+++ Data/GraphProvider.cs
@@ -0,0 +1,11 @@
+﻿using Plotly.Blazor;
+
+namespace Squill.Data;
+
+public abstract class GraphProvider
+{
+    public PlotlyChart Chart { get; set; }
+    public Config Config { get; set; }
+    public Layout Layout { get; set; }
+    public abstract IList<ITrace> Data { get; }
+}
